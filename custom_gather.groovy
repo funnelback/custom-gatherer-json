cb020de530713087886a1c0b55ae56100de92502
Peter Levan <plevan@peter-levans-macbook-pro.local>
2016-06-22 15:20:21 +1000

    PL: initial import

---
+++ custom_gather.groovy
@@ -0,0 +1,44 @@
+import com.funnelback.common.*;
+import com.funnelback.common.config.*;
+import com.funnelback.common.io.store.*;
+import com.funnelback.common.io.store.xml.*;
+import com.funnelback.common.utils.*;
+import java.net.URL;
+
+//JSON imports
+import org.json.*;
+
+// Read $SEARCH_HOME
+def searchHome = Environment.getValidSearchHome().getCanonicalPath();
+
+// Create a configuration object to read collection.cfg
+def config = new NoOptionsConfig(new File(args[0]), args[1]);
+
+// Create a Store instance to store gathered data
+def store = new XmlStoreFactory(config).newStore();
+
+// Open the XML store
+store.open()
+// Open the start URLs file
+File file = new File(searchHome+File.separatorChar+"conf"+File.separatorChar+config.value("collection")+File.separatorChar+"collection.cfg.start.urls")
+
+def line
+file.withReader { reader ->
+    while ((line = reader.readLine())!=null) {
+
+        println "Gathering JSON for "+line;
+
+        // Fetch the JSON file and convert it to XML
+        def jsonText = new URL(line).getText();
+        //println "JSON: "+jsonText;
+        JSONObject json = new JSONObject(jsonText)
+        String xml = XML.toString(json)
+        //println "XML:"+xml
+        println "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<json>\n"+xml+"\n</json>"
+        def xmlContent = XMLUtils.fromString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<json>"+xml+"\n</json>")
+
+        store.add(new XmlRecord(xmlContent, line))
+    }
+}
+// close() required for the store to be flushed
+store.close()
